f0b3e34c8e7dc14735e6793683d3976c4a45f14f
Andres Castillo <andres.castillo@mail.utoronto.ca>
2025-07-03 23:35:23 -0400

    NOT WORKING - Implement battery % on display

--- custom_status_screen.c
+++ custom_status_screen.c
@@ -1,5 +1,6 @@
 #include <zephyr/kernel.h>
 #include <lvgl.h>
+#include <zmk/battery.h>
 
 lv_obj_t *zmk_display_status_screen() {
     lv_obj_t *screen = lv_obj_create(NULL);
@@ -7,6 +8,11 @@ lv_obj_t *zmk_display_status_screen() {
     lv_obj_t *hello_label = lv_label_create(screen);
     lv_label_set_text(hello_label, "Hello MKBD!");
     lv_obj_align(hello_label, LV_ALIGN_CENTER, 0, 0);
+    
+    lv_obj_t *battery_label = lv_label_create(screen);
+    uint8_t battery_level = zmk_battery_state_of_charge();
+    lv_label_set_text_fmt(battery_label, "%d%%", battery_level);
+    lv_obj_align(battery_label, LV_ALIGN_BOTTOM_LEFT, 5, -5);
     
     return screen;
 }
